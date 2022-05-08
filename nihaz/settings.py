de070dc47693c903cbc5a800a0e2c7dc09083885
Your Name <sudaniy65@gmail.com>
2022-05-08 18:35:10 +0100

    heroku

--- nihaz/settings.py
+++ nihaz/settings.py
@@ -15,6 +15,9 @@ import os
 from urllib.parse import urlparse
 from django.core.management.utils import get_random_secret_key
 
+import django_heroku
+django_heroku.settings(locals())
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
